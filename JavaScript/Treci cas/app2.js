3543a07a1f06731b695a26455d13ce4056c32f8b
MrNikola <nikola.bozovic.np@gmail.com>
2022-10-18 21:10:48 +0200

    3

--- JavaScript/Treci cas/app2.js
+++ JavaScript/Treci cas/app2.js
@@ -1,2 +1,41 @@
 // OERATORI U JAVASCRIPTU //
 // Podelicemo operatore u 6 grupa:
+
+// 1. Aritmeticki operatori (Artimetic operators);
+// 2. Operatori dodele vrednosti (Assigment operators)
+// 3. String operatori (String operators)
+// 4. Operator poredjenja (Comparison operators);
+// 5. Logicki operatori (Logical operators);
+// 6. Tip operatori  (Type operators);
+
+// 1. Aritmeticki operatori (Artimetic operators);
+// 1.1 + operator sabiranja
+console.log(4 + 5);
+// 1.2 - operator oduzimanja
+console.log(4 - 5);
+// 1.3 * operator mnozenja
+console.log(4 * 5);
+// 1.4 / operator deljenja
+console.log(20 / 5);
+// 1.5 ** operator stepenovanja
+console.log(2 ** 3);
+// 1.6 % Modulo (Ostatak pri deljenju dva broja)
+console.log(11 % 3); // 2
+console.log(12 % 3); // 0
+
+// Uslov za parnost :
+// i % 2 = 0
+// Uslov za neparnost :
+// i % 2 = 1
+
+// 1.5 ++ Increment (Povecava vrednost za 1)
+let x = 5;
+// post fiksni operator
+x++; // x = x + 1
+console.log(x);
+
+let y = 11;
+// prefiksni operator
+console.log(++y);
+
+// 1.8 -- Decrement (Smanjuje vrednost za 1)
