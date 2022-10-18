f112f1eed7240c0a8c16171c29f487987717556c
MrNikola <nikola.bozovic.np@gmail.com>
2022-10-18 19:53:30 +0200

    java 3.

---
+++ JavaScript/Drugi cas/app.js
@@ -0,0 +1,40 @@
+// JavaScrip moze prikazati podatke na nekoliko nacina :
+
+// 1. innerHTML
+document.getElementById("p").innerHTML = "Paragraf";
+
+// 2. write()
+document.write(); // najcesci nacin za testiranje nekog koda
+
+// 3. alert() -upozorenje
+alert("Zdravo");
+
+//4. console.log() - ispisivanje u konzoli
+console.log("Poruka za ispis u konzoli");
+
+// Pravila nazivanja promenjivih:
+
+//Promenjiva u JavaScriptu mora poceti sa nekim od sledecih
+
+// 1. Slovo (veliko ili malo) A-Z, a-z
+// 2. $ znak moze biti pocetni karakter neke promenljive,
+// 3. _
+
+// Broj moze biti sadrzan u nazivu neke promenljive
+
+// 2prom = 4.14
+prosekOcena = 4.14; // preporucljiv nacin zapisivanja promenljivih
+prosek_ocena = 4.14; // korektno
+ProsekOcena = 4.14; // korektno
+
+// Postoje 4 nacina za deklarisanje neke promenjive:
+
+// 1. Dodeljivanje vrednosti bezda smo prethodno izvrsili da
+// prom = 12
+
+// 2. Koriscenjem var keyword (rezervisane reci) -
+// var je koriscena na starijim browserima (sve se manje koriisti)
+// var a = 5
+
+// 3. Koriscenjem let keyword (rezervisane reci) -
+//
