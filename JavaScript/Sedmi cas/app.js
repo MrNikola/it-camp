3993f06a682dfed3eaa1e6825da0804e6cf87d97
MrNikola <nikola.bozovic.np@gmail.com>
2022-10-29 19:19:09 +0200

    s

---
+++ JavaScript/Sedmi cas/app.js
@@ -0,0 +1,45 @@
+// while petlja nam omogucava iteraciju kroz neki objekat
+// nekoliko puta uz potencijalnoodreljene yslove.
+// kod while petlje  je neophodno nakraju kada definisati
+// povecanjeiteratora. (Kako nebismobeskonacnu pet)
+
+// Sintaksa
+
+// while (uslov) {
+// blok koda za izvrsavanja
+// definisati povecanje iteratora
+// }
+
+// Ispisati sve brojeve od 1 do 10 koji su parni
+// let i = 0;
+// while (i < 11) {
+//  if (1 % 2 === 0) {
+//    console.log(i);
+//  }
+//  i++;
+// }
+
+// Traziti od korisnika da unese neki broj. Na osnovu te
+//vrednosti izvrsiti ispis kvadrata brojeva od 1 do
+//tog unetog broja:
+
+// const broj = +prompt("Unesite neki pozitivan broj : ");
+// let iterator = 1 ;
+// while (iterator <= broj) {
+//  console.log(iterator ** 2);
+//  iterator++;
+// }
+
+// Korisnik unosi 2 broja:
+// Na osnovu toga koji je broj mani, iteracija se vrsi od
+// njega do veceg broja.
+// Ispisuju se brojevi od manjeg ka vecem.
+
+let broj1 = +prompt("Unesite prvi broj : ");
+let broj2 = +prompt("Unesite drugi broj : ");
+if (broj1 < broj2) {
+while (broj1 <= broj2)
+} else if (broj1 > broj2) {
+
+}
+
