996928fbb7ff4606c5aad161ce8663ac0c5cd6aa
MrNikola <nikola.bozovic.np@gmail.com>
2022-11-08 20:47:42 +0100

    dgz

--- JavaScript/Treci cas/app.js
+++ JavaScript/Treci cas/app.js
@@ -1,6 +1,6 @@
 // 1. var
-var x; // deklarisanje promenjive x (obezbedjivanje lokacijske memorije za promenjivu x)
-x = 15; // inicijalizacija promenjive x (dodela vrednosti vec deklarisanoj promenjive)
+var x; // deklarisanje promenljive x (obezbedjivanje lokacijske memorije za promenljivu x)
+x = 15; // inicijalizacija promenljive x (dodela vrednosti vec deklarisanoj promenljivoj x)
 // var x = 15 - takodje dozvoljeno
 console.log(x);
 
@@ -11,15 +11,15 @@ var x;
 // bilo gde u prostoru.
 x = 22;
 
-// 2. let - Definisanje promenljive za koju postoji mogucnost promene vrednosti
-let y; // deklarisanje promenjive y (obezbedjivanje lokacijske memorije za promenjivu x)
-y = 14; // inicijalizacija promenjive y (dodela vrednosti vec deklarisanoj promenjive)
-// let y = 14
+// 2. let - Definisanje promenljive za koju postoji mogucnost promene vrednosti.
+let y; // deklarisanje promenljive y (obezbedjivanje lokacijske memorije za promenljivu y)
+y = 14; // inicijalizacija promenljive y (dodela vrednosti vec deklarisanoj promenljivoj y)
+// let y = 14; - takodje dozvoljeno
 console.log(y);
 
 let a, b, c; // korektan nacin deklarisanja promenljivih
 
-// Redeklarisanje definisane putem left keyword nije dozvoljeno ako govorimo o istom prostoru
+// Redeklarisanje promenljive definisane putem let keyword nije dozvoljeno
 // ako se radi o istom prostoru.
 // let y;
 
@@ -31,16 +31,16 @@ console.log(y);
 // Promenljive koje su definisane putem let ili const keyword nam obezbedjuju
 // Block Scope.
 {
-  //  console.log(y); - prepoznanje se promenljiva iz Global Scope.
-  // Redeklaracija promenljive definisane putem let, u drugom prostoru je dozvoljena.
+  //   console.log(y); - prepoznaje se promenljiva iz Global Scope.
+  //   Redeklaracija promenljive definisane putem let, u drugom prostoru je dozvoljena.
   let y = 19;
   console.log(y);
 }
-// Ispisuje se 12, odnosno poslednja dodeljena vrednost unutar Global Scope.
+//  Ispisuje se 12, odnosno poslednje dodeljena vrednost unutar Global Scope.
 console.log(y);
 
-// 3. counst
-// Promenljive definisane putem const moraju biti odjednom deklarisane i inicija
+// 3. const
+// Promenljive definisane putem const moreju biti odjednom deklarisane i inicijalizovane.
 // const z;
 // z = 26;
 const z = 26;
@@ -52,12 +52,10 @@ console.log(z);
 // kada se radi o istom prostoru.
 // const z = 23;
 
-// Redeklarisanje promenljive definisane putem const nije dozvoljena,
+// Reinicijalizacija promenljive definisane putem const nije dozvoljena,
 // kada se radi o istom prostoru.
-// z = 23
+// z = 23;
 // console.log(z);
 
-// Sve karakteristike koje vaze za promenljive definisane putem let
-// u Block Scope, vaze i za promenljive definisane putem const keyword.
-
-const cars = ["Audi", "Golf", "Mercedes", "BMW", "Nissan"];
+// Sve karakteristike koje vaze za promenljive definisane putem let keyword
+// u Block Scope, vaze i za proemnljive definisane putem const keyword.
