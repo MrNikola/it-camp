c71812b3bda75e0c05649ccc199e796e61975b8f
MrNikola <nikola.bozovic.np@gmail.com>
2022-12-13 19:36:37 +0100

    fd

--- JavaScript/Dvadeset treci cas/app.js
+++ JavaScript/Dvadeset treci cas/app.js
@@ -15,3 +15,38 @@ const x = 0.1;
 const y = 0.2;
 const z = x + y;
 console.log(z);
+
+// + operator:
+console.log("10" + 10); // "1010"
+
+console.log("10" - 2); // 8
+
+// NaN - Not a Number
+// NaN je JavaScript rezervisana rec koja ukazuje da broj nije korektan.
+// Nan je tipa number !!!
+
+// Za proveru da li je neka vrednost korektan broj se koristi !isNaN()
+
+console.log(isNan(100 / "Apple"));
+
+// Infinity (ili -Infinity) je vrednost kojucemoda dobijamo kada rezultat premasi najveci broj u JavaScriptu(bude veoma mali broj).
+
+const a = 25;
+const c = -1;
+const b = 0;
+
+console.log(a / b);
+console.log(c / b);
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+
+// rgb(0, 255, 123)
+// #12ab33
+// rgby
+
+// Hexadecimal numbers:
+// Ako su brojevi napisani prefiksom 0xFF rezultat ce biti 255.
+
+const r = 0xff;
+console.log(r);
