7f04b58ab9bf82b452db2e92c55724210fab0314
MrNikola <nikola.bozovic.np@gmail.com>
2022-12-13 18:55:03 +0100

    s

---
+++ JavaScript/Dvadeset treci cas/app.js
@@ -0,0 +1,15 @@
+// JavaScript prepoznaje samo jedan tip podatka za brojeve.
+// U pitanju je number tip podatka i oni mogu biti zapisani sa ili bez decimalnog zareza.
+
+// Veliki brojevi mogu biti zapisani sa tzv. exponent oznakom:
+
+const petica = 5e6;
+console.log(petica);
+
+const noviBroj = 555e-5;
+console.log(noviBroj);
+
+const x = 0.1;
+const y = 0.2;
+const z = x + y;
+console.log(z);
