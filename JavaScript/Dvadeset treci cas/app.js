f3a65c508d30b9a276024d3f2eb6ffc0ac445802
MrNikola <nikola.bozovic.np@gmail.com>
2022-12-20 19:51:15 +0100

    sd

--- JavaScript/Dvadeset treci cas/app.js
+++ JavaScript/Dvadeset treci cas/app.js
@@ -10,7 +10,6 @@ const noviBroj = 555e-5;
 console.log(noviBroj);
 
 // Sto se tice sabiranja decimalnih brojeva JavaScript nije bas najbolji kalkulator.
-
 const x = 0.1;
 const y = 0.2;
 const z = x + y;
@@ -23,14 +22,13 @@ console.log("10" - 2); // 8
 
 // NaN - Not a Number
 // NaN je JavaScript rezervisana rec koja ukazuje da broj nije korektan.
-// Nan je tipa number !!!
+// NaN je tipa number!!!
 
-// Za proveru da li je neka vrednost korektan broj se koristi !isNaN()
+// Za proveru da li je neka vrednost korektan broj se koristi !isNaN().
 
-console.log(isNan(100 / "Apple"));
+console.log(isNaN(100 / "Apple"));
 
-// Infinity (ili -Infinity) je vrednost kojucemoda dobijamo kada rezultat premasi najveci broj u JavaScriptu(bude veoma mali broj).
-
+// Infinity (ili -Infinity) je vrednost koju cemo da dobijemo kada rezultat premasi najveci broj u JavaScriptu  (bude veoma mali broj).
 const a = 25;
 const c = -1;
 const b = 0;
@@ -43,10 +41,40 @@ console.log(typeof -Infinity);
 
 // rgb(0, 255, 123)
 // #12ab33
-// rgby
+// rgb(18, 171, 51)
+
+// 12 = 1*16 + 2*1 = 18
+// ab = 10*16 + 11*1 = 171
+// 33 = 3*16 + 3*1 = 48 + 3 = 51
 
 // Hexadecimal numbers:
 // Ako su brojevi napisani prefiksom 0xFF rezultat ce biti 255.
 
 const r = 0xff;
 console.log(r);
+
+// Pored osnovnog nacina dodeljivanja vrednosti nekoj promenljivoj mozemo to uraditi i preko new:
+// Nije bas dobra praksa.
+const k = 22;
+const l = new Number(33);
+
+console.log(k);
+console.log(l);
+
+console.log(k + l);
+
+console.log(typeof k);
+console.log(typeof l);
+
+// Napraviti funkciju koja:
+// a) pretvara km u m
+// b) pretvara inch u cm
+
+function kmToM(km) {
+  return km * 1000;
+}
+
+const inchToCm = (inch) => inch * 2.54;
+
+console.log(kmToM(4.68));
+console.log(inchToCm(43));
