66a7a8bdd4bac1d146213216a26f17931bf26f07
MrNikola <nikola.bozovic.np@gmail.com>
2022-12-22 20:26:30 +0100

    zas

--- JavaScript/Dvadeset sesti cas/app.js
+++ JavaScript/Dvadeset sesti cas/app.js
@@ -2,4 +2,54 @@
 
 // Math Objekat nam dozvoljava da izvrsimo neke dodatne radnje sa brojevima.
 
-//
+// Math Properties (Konstante)
+
+// Sintaksa je Math.Property.
+
+// Math konstanti ima 8:
+
+// 1. Math.E - vraca Ojlerov Broj
+console.log(Math.E);
+
+// 2. Math.PI - vraca vrednost pi
+console.log(Math.PI);
+
+// 3. Math.SQRT2 - vraca vrednost kvadratnog korena broja 2
+console.log(Math.SQRT2);
+
+// 4. Math.SQRT1_2 - vraca vrednost kvadratnog korena broja 1/2
+console.log(Math.SQRT1_2);
+
+// 5. Math.LN2 - vraca vrednost logoritma osnove e broja 2
+console.log(Math.LN2);
+
+// 6. Math.LN10 - vraca vrednost logoritma osnove e broja 10
+console.log(Math.LN10);
+
+// 7. Math.LOG2E - vraca vrednost logoritma osnove 2 broja e
+console.log(Math.LOG2E);
+
+// 8. Math.LOG10E - vraca vrednost logoritma osnove 10 broja e
+console.log(Math.LOG10E);
+
+// Math Methods //
+
+// Sintaksa je:
+// Math.method(number)
+
+// Number to Integer:
+
+// 1. Math.round() - vraca blizi ceo broj
+
+console.log(Math.round(22.23));
+console.log(Math.round(22.74));
+
+// 2. Math.ceil() - vraca sledeci ceo broj
+
+console.log(Math.ceil(22.22));
+console.log(Math.ceil(-22.22));
+
+// 3. Math.floor() - vraca predhodni ceo broj
+
+console.log(Math.floor(22.22));
+console.log(Math.ceil(-22.22));
