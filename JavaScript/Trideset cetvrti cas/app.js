45c8a8d093b340afbfa0b847f46a5decf3fe9efa
MrNikola <nikola.bozovic.np@gmail.com>
2023-01-12 19:54:16 +0100

    cs

--- JavaScript/Trideset cetvrti cas/app.js
+++ JavaScript/Trideset cetvrti cas/app.js
@@ -1,5 +1,7 @@
 // callback funkcije //
 
+// callback funkcija predstavlja funkciju koju saljemo kao argument neke druge funkcije.
+// Sam argument ne treba da bude izvrsavanje
 function glavnaFunkcija(func) {
   console.log("Radnja unutar glavne funkcije.");
   func();
