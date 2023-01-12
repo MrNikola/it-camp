2f45d548750b55add83c847811607532e8ca4a52
MrNikola <nikola.bozovic.np@gmail.com>
2023-01-12 20:10:51 +0100

    sx

--- JavaScript/Trideset cetvrti cas/app.js
+++ JavaScript/Trideset cetvrti cas/app.js
@@ -2,6 +2,7 @@
 
 // callback funkcija predstavlja funkciju koju saljemo kao argument neke druge funkcije.
 // Sam argument ne treba da bude izvrsavanje
+
 function glavnaFunkcija(func) {
   console.log("Radnja unutar glavne funkcije.");
   func();
@@ -11,6 +12,18 @@ function glavnaFunkcija(func) {
 function callback() {
   console.log("Radnja unutar callback funkcije.");
 }
+
 glavnaFunkcija(callback);
 
-// Napraviti glavnu funkciju unutar koje ce se desiti radnja za ispis
+// Napraviti glavnu funkciju unutar koje ce se desiti ispis sta ona predstavlja, a celu stvar ce da izvrsi callback funkcija. Callback funkcija ima 2 parametra, i ispisuje rezultat par1 stepenovan pa2.
+
+const mainFunction = (callback2) => {
+  console.log("Prvi broj stepenovan drugim je:");
+  callback2(2, 3);
+};
+
+const callBack2 = (par1, par2) => {
+  console.log(par1 ** par2);
+};
+
+mainFunction(callBack2);
