957ffea42a26c05377342c9cb4af3cf9d12e3277
MrNikola <nikola.bozovic.np@gmail.com>
2023-01-28 18:17:27 +0100

    adv

--- JavaScript/Cetrdeset prvi cas/app1.js
+++ JavaScript/Cetrdeset prvi cas/app1.js
@@ -1,1 +1,37 @@
 // JS objekti su mutable (promenljivi) //
+
+const dzenan = {
+  ime: "Dzenan",
+  prezime: "Mecinovic",
+  godine: 19,
+};
+
+const dzenan2 = dzenan;
+
+console.log(dzenan2);
+dzenan2.prezime = "Kosuta";
+dzenan2.godine = 26;
+console.log(dzenan2);
+console.log(dzenan);
+
+// Brisanje svojstava iz objekta:
+
+const mitar = {
+  ime: "Mitar",
+  prezime: "Vranic",
+  godine: 20,
+  skola: "Prva Tehnicka",
+};
+
+// delete mitar.skola; // delete keyword se koristi za brisanje elementa
+// delete obj.property brise i key ivalue
+console.log(mitar);
+
+// 2. nacin
+// delete mitar["skola"]
+
+// Napomena.
+// Vrednosti nekog objekta mogu biti:
+// Objekti,
+// Nizovi,
+// Funkcije...
