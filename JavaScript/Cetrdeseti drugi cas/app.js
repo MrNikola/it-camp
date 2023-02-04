55c4fa105f6a96884f30ccf1dc037ade9fb8af77
MrNikola <nikola.bozovic.np@gmail.com>
2023-02-04 17:36:33 +0100

    as

---
+++ JavaScript/Cetrdeseti drugi cas/app.js
@@ -0,0 +1,110 @@
+// Object build in Methods //
+
+const person = {
+  firstName: "Hamed",
+  lastName: "Madjedovic",
+  age: 16,
+};
+
+// Object.freeze(obj) metoda cini objekat nepromenljivim.
+person.age = 17;
+console.log(person);
+// Object.freeze(person);
+// person.age = 22;
+// console.log(person);
+
+// Object.seal(obj) cini objekat promenljivim s tim sto nema mogucnost dodavanja novih svojstava i metoda.
+
+// Object.seal(person);
+// person.age = 22;
+// console.log(person);
+// delete person.age;
+// console.log(person);
+
+// Object.keys(obj) vraca niz kljuceva:
+console.log(Object.keys(person));
+
+// Object.entries(obj) vraca niz nizova:
+// Svaki podniz je niz oddva elementa.
+const entries = Object.entries(person);
+console.log(entries);
+
+const newArr = entries.flat().filter((element) => typeof element === "string");
+console.log(newArr);
+
+// Object.is(obj1, obj2)
+
+const obj1 = {
+  firstName: "Mitar",
+  lastName: "Vranic",
+};
+
+const obj2 = {
+  firstName: "Mitar",
+  lastName: "Vranic",
+};
+const obj3 = obj1;
+
+console.log(Object.is(obj1, obj2));
+console.log(Object.is(obj1, obj3));
+
+const bakir = "Bakir";
+const ujkanovic = "Ujkanovic";
+const godine = 19;
+
+const person2 = {
+  ime,
+  prezime,
+  godine,
+};
+console.log(person2);
+
+// Destructuring
+
+const person3 = {
+  firstName: "Hasan",
+  lastName: "Mujanovic",
+  age: 18,
+};
+
+// const firstName = person3.firstName;
+// console.log(firstName);
+
+const { firstName, age } = person3;
+console.log(firstName);
+console.log(age);
+
+const godina = [
+  { starost: 19 },
+  { starost: 19 },
+  { starost: 19 },
+  { starost: 18 },
+  { starost: 16 },
+  { starost: 18 },
+  { starost: 14 },
+  { starost: 17 },
+];
+// Napraviti novi niz(punoletnih) koji ce sadrzati one elemente (objekte) cija je starsot veca od 17.
+
+const newArr1 = godina.filter((element) => element.starost > 17);
+console.log(newArr1);
+
+//Write a JavaScript function to sort the following array of objects by title value.
+
+const library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    libraryID: 1254,
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    libraryID: 4264,
+  },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    libraryID: 3245,
+  },
+];
