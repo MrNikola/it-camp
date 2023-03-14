903c87fcb7b659c00ee97ec417c358c0f8a64824
MrNikola <nikola.bozovic.np@gmail.com>
2023-03-14 20:23:47 +0100

    +9

---
+++ JavaScript/Pedeseti cas/app.js
@@ -0,0 +1,15 @@
+function func1() {
+  console.log("func1");
+  console.log("func1");
+  console.log("func1");
+  console.log("================");
+}
+function func2() {
+  console.log("func2");
+  console.log("func2");
+  console.log("func2");
+  console.log("================");
+}
+
+["Telescopers", "Glasses", "Eyes", "Monocles"];
+function funkcija(niz) {}
